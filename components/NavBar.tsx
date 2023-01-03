bdb98735458f468ed9b0d4654a7d3b8677421042
dvher <diego.vergara5@mail.udp.cl>
2023-01-02 23:40:58 -0300

    Changed font of logo

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,7 +1,10 @@
-import { AppBar, Toolbar, Typography, Menu, MenuItem, Box, IconButton, Button } from "@mui/material";
+import { AppBar, Toolbar, Typography, Menu, MenuItem, Box, IconButton, Button, Container, InputBase } from "@mui/material";
 import Link from 'next/link';
 import MenuIcon from '@mui/icons-material/Menu';
+import AdbIcon from '@mui/icons-material/Adb';
+import SearchIcon from '@mui/icons-material/Search';
 import { useState } from "react";
+import { styled, alpha } from '@mui/material/styles';
 
 export default function NavBar() {
 
@@ -17,60 +20,147 @@ export default function NavBar() {
 
     const pages = [["Iniciar Sesión", "/login"], ["Sobre nosotros", "/about"], ["Contacto", "/contact"]];
 
+    const Search = styled('div')(({ theme }) => ({
+      position: 'relative',
+      borderRadius: theme.shape.borderRadius,
+      backgroundColor: alpha(theme.palette.common.white, 0.15),
+      '&:hover': {
+        backgroundColor: alpha(theme.palette.common.white, 0.25),
+      },
+      marginLeft: 0,
+      width: '100%',
+      [theme.breakpoints.up('sm')]: {
+        marginLeft: theme.spacing(1),
+        width: 'auto',
+      },
+    }));
+    
+    const SearchIconWrapper = styled('div')(({ theme }) => ({
+      padding: theme.spacing(0, 2),
+      height: '100%',
+      position: 'absolute',
+      pointerEvents: 'none',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    }));
+    
+    const StyledInputBase = styled(InputBase)(({ theme }) => ({
+      color: 'inherit',
+      '& .MuiInputBase-input': {
+        padding: theme.spacing(1, 1, 1, 0),
+        // vertical padding + font size from searchIcon
+        paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+          width: '12ch',
+          '&:focus': {
+            width: '20ch',
+          },
+        },
+      },
+    }));
+
     return (
         <AppBar position="static" sx={{
             backgroundColor: "#86E3CE",
         }}>
+          <Container maxWidth="xl">
             <Toolbar disableGutters>
-                <Typography variant="h6" component="div" sx={{ flexGrow: 0 }}>
+              <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+              <Typography
+                variant="h6"
+                noWrap
+                component="a"
+                href="/"
+                sx={{
+                  mr: 2,
+                  display: { xs: 'none', md: 'flex' },
+                  fontFamily: 'monospace',
+                  fontWeight: 700,
+                  letterSpacing: '.3rem',
+                  color: 'inherit',
+                  textDecoration: 'none',
+                }}
+              >
                 Nibbin
-                </Typography>
+              </Typography>
                 <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-                <IconButton
-                  size="large"
-                  aria-label="account of current user"
-                  aria-controls="menu-appbar"
-                  aria-haspopup="true"
-                  onClick={handleOpen}
-                  color="inherit"
-                >
-                  <MenuIcon />
-                </IconButton>
-                <Menu
-                  id="menu-appbar"
-                  anchorEl={anchorEl}
-                  anchorOrigin={{
-                    vertical: 'bottom',
-                    horizontal: 'left',
-                  }}
-                  keepMounted
-                  transformOrigin={{
-                    vertical: 'top',
-                    horizontal: 'left',
-                  }}
-                  open={Boolean(anchorEl)}
-                  onClose={handleClose}
-                  sx={{
-                    display: { xs: 'block', md: 'none' },
-                  }}
-                >
-                  {pages.map(([page, url]) => (
-                    <MenuItem key={page} onClick={handleClose}>
-                      <Link href={url}>{page}</Link>
-                    </MenuItem>
-                  ))}
-                </Menu>
+                  <IconButton
+                    size="large"
+                    aria-label="account of current user"
+                    aria-controls="menu-appbar"
+                    aria-haspopup="true"
+                    onClick={handleOpen}
+                    color="inherit"
+                  >
+                    <MenuIcon />
+                  </IconButton>
+                  <Menu
+                    id="menu-appbar"
+                    anchorEl={anchorEl}
+                    anchorOrigin={{
+                      vertical: 'bottom',
+                      horizontal: 'left',
+                    }}
+                    keepMounted
+                    transformOrigin={{
+                      vertical: 'top',
+                      horizontal: 'left',
+                    }}
+                    open={Boolean(anchorEl)}
+                    onClose={handleClose}
+                    sx={{
+                      display: { xs: 'block', md: 'none' },
+                    }}
+                  >
+                    {pages.map(([page, url]) => (
+                      <MenuItem key={page} onClick={handleClose}>
+                        <Link href={url}>{page}</Link>
+                      </MenuItem>
+                    ))}
+                  </Menu>
               </Box>
+              <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+              <Typography
+                variant="h5"
+                noWrap
+                component="a"
+                href=""
+                sx={{
+                  mr: 2,
+                  display: { xs: 'flex', md: 'none' },
+                  flexGrow: 1,
+                  fontFamily: 'monospace',
+                  fontWeight: 700,
+                  letterSpacing: '.3rem',
+                  color: 'inherit',
+                  textDecoration: 'none',
+                }}
+              >
+                Nibbin
+              </Typography>
               <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
                 {pages.map(([page, url]) => (
-                    <Button key={page} onClick={handleClose}>
+                    <Button key={page} onClick={handleClose} sx={{ my: 2, color: 'white', display: 'block' }}>
                       <Link href={url}>
-                        <Typography sx={{ my: 2, color: 'white', display: 'block' }}>{page}</Typography>
+                        <Typography>{page}</Typography>
                       </Link>
                     </Button>
                 ))}
               </Box>
+              <Search>
+                <SearchIconWrapper>
+                  <SearchIcon />
+                </SearchIconWrapper>
+                <StyledInputBase
+                  placeholder="Search…"
+                  inputProps={{ 'aria-label': 'search' }}
+                />
+              </Search>
             </Toolbar>
+          </Container>
         </AppBar>
     );
 }
