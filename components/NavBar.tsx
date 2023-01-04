08861830fb88f9ba56451e994fabd9a323227b34
dvher <diego.vergara5@mail.udp.cl>
2023-01-04 16:31:38 -0300

    Added searchbar for navbar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -150,7 +150,7 @@ export default function NavBar() {
                     </Button>
                 ))}
               </Box>
-              <Search>
+              <Search sx={{ display: { xs: 'none', md:'flex' }}}>
                 <SearchIconWrapper>
                   <SearchIcon />
                 </SearchIconWrapper>
